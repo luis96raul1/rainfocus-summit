5217b025fa1a4e4a9a221db283b64ac0c69744d9
Luis <luis96raul1@gmail.com>
2023-04-26 17:30:08 -0500

    resized for mobile

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { RainFocusLogo, SummitLogo, User } from "../../utils/images";
+import { LogicIcon, RainFocusLogo, SummitLogo, User } from "../../utils/images";
 
 import "./sidebar.scss";
-import { ReducedSidebarInfo } from "./sidebarInfo/ReducedSidebarInfo";
 import { SummitInfoSidebar } from "./sidebarInfo/SummitInfoSidebar";
 
 export const SideBar = () => {
@@ -30,14 +29,20 @@ export const SideBar = () => {
           <div onClick={handleSidebarInfoStatus}>
             <img src={SummitLogo} alt="Summit" height={32} width={32} />
           </div>
+          {isSidebarClosed && (
+            <div
+              className="sidebar-back-button"
+              onClick={handleSidebarInfoStatus}
+            >
+              <img src={LogicIcon} alt="logic arrow" />
+            </div>
+          )}
         </div>
         <div className="user-icon">
           <img src={User} alt="User" height={32} width={32} />
         </div>
       </div>
-      {isSidebarClosed ? (
-        <ReducedSidebarInfo setSidebarStatus={setSidebarStatus} />
-      ) : (
+      {!isSidebarClosed && (
         <SummitInfoSidebar setSidebarStatus={setSidebarStatus} />
       )}
     </>
